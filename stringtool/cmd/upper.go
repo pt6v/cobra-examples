9399b6e60bcc4e569ccef543477ffac4dca879a2
Alvin Zhang <zhsh411@126.com>
2019-10-29 11:42:25 +0800

    feat(stringtool): add debug to show detail info

--- stringtool/cmd/upper.go
+++ stringtool/cmd/upper.go
@@ -2,24 +2,25 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"strings"
 )
 
 var upperCmd = &cobra.Command{
-	Use:   "upper",
+	Use:   "upper [str]",
 	Short: "return string uppercase",
 	Long:  "return the certain string uppercase",
-
 	Run:   runUpper,
 }
 
 func runUpper(cmd *cobra.Command, args []string) {
+	logrus.Debug("args", args)
 	if len(args) >= 1 {
 		str := args[0]
 		fmt.Println(strings.ToUpper(str))
 	} else {
-		fmt.Println("String is missing")
+		logrus.Error("String is missing")
 	}
 }
 
--- stringtool/go.mod
+++ stringtool/go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/huandu/xstrings v1.2.0
+	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.4.0
 )
